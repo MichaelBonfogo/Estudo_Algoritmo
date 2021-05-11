e333da49c24b99b9662589a80a11ff09953ff11e
Michael Douglas Bonfogo <59799217+MichaelBonfogo@users.noreply.github.com>
2021-05-11 00:35:53 -0300

    Update infoResumoTeorico.js
    Update das informações.

--- infoResumoTeorico.js
+++ infoResumoTeorico.js
@@ -237,12 +237,55 @@ console.log(saudacao);
 /*------------------------------------------------------------------------------*/
 
 // 96. Extra: Template String
+// (`i = ${i}, j = ${j}`)
+
+const i = 2;
+const j = 4;
+
+console.log("i = " + i + ", j = " + j);
+console.log('i = ' + i + ', j = ' + j);
+
+console.log(`i = ${i}, j = ${j}`);
+console.log(`1 + 2 = ${1 + 2}`);
+console.log(`Valor gerado foi: ${Math.random()}`);
+
+console.log("i = ${i}, j = ${j}");
+console.log('i = ${i}, j = ${j}');
 
 /*------------------------------------------------------------------------------*/
 
 // Seção 6: Funções
 
-// 
+// #1: Função COM Parâmetros e COM Retorno
+function somar(a, b) {
+    return a + b;
+}
+
+let resultado = somar(30, 56);
+console.log(`O resultado final é: ${resultado}`);
+console.log(`O resultado final é: ${somar(30, 56)}`);
+
+// #2: Função COM Parâmetros e SEM Retorno
+function exibirMultiplicacao(a, b) {
+    console.log(a * b);
+}
+
+exibirMultiplicacao(10, 21);
+exibirMultiplicacao(7, 9);
+
+// #3: Função SEM Parâmetros e COM Retorno
+function retornarDataAtual() {
+    return new Date();
+}
+
+console.log(retornarDataAtual());
+
+// #4: Função SEM Parâmetros e SEM Retorno
+function exibirHoraAtual() {
+    console.log(new Date().getHours());
+}
+
+exibirHoraAtual();
 
 /*------------------------------------------------------------------------------*/
 
